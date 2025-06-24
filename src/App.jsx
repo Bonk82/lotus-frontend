971b9f5575ce8502ed6bed4d88aa821676e6dcf0
Cristian Alvarado <alvarado@upre.gob.bo>
2025-06-24 12:10:34 -0400

    'sesion90%'

--- src/App.jsx
+++ src/App.jsx
@@ -16,61 +16,70 @@ import Parametrica from './pages/Parametrica';
 
 import { AppShell } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import Navbar from './components/Navbar';
 // import { MantineLogo } from '@mantinex/mantine-logo';
 
 const myTheme =createTheme({
   primaryColor:'violet',
+  secondaryColor:'grape',
   fontFamily:'Michroma, sans-serif',
 })
 
 
 function App() {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
-  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
-
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(false);
+  let inicio = false;
+  console.log('appshell',window.location.pathname);
+  if(window.location.pathname == '/login') inicio = true;
+  
   return (
     <AuthProvider>
       <MantineProvider defaultColorScheme="dark" forceColorScheme="dark" theme={myTheme}>
         <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-      }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-          LOOOGO{/* <MantineLogo size={30} /> */}
-        </Group>
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
-      </AppShell.Navbar>
-      <AppShell.Main>
-        <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/" element={<Dashboard/>} />
-          <Route path="/proveedor" element={<ProtectedRoute><Proveedor/></ProtectedRoute>} />
-          <Route path="/inventario" element={<ProtectedRoute><Inventario/></ProtectedRoute>} />
-          <Route path="/personal" element={<ProtectedRoute><Personal/></ProtectedRoute>} />
-          <Route path="/pedido" element={<ProtectedRoute><Pedido/></ProtectedRoute>} />
-          <Route path="/caja" element={<ProtectedRoute><Caja/></ProtectedRoute>} />
-          <Route path="/control" element={<ProtectedRoute><Control/></ProtectedRoute>} />
-          <Route path="/parametrica" element={<ProtectedRoute><Parametrica/></ProtectedRoute>} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-      </AppShell.Main>
-    </AppShell>
-        
+          header={{ height: 60 }}
+          navbar={{
+            width: 300,
+            breakpoint: 'sm',
+            collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+          }}
+          padding="md"
+          transitionDuration={700}
+          transitionTimingFunction='ease'
+        >
+          <AppShell.Header style={{ backgroundColor: 'black', color: '#9775fa' , borderBottom: '1px solid #9775fa'}}>
+            <Group h="100%" px="md" style={{ justifyContent: 'space-between'}} color='primary'>
+              <Group align="center" h="100%" gap="s" style={{ border:'1px solid black', color: '#9775fa', fontSize: '1.2rem', fontWeight: 'bold' }}>
+                <Burger color="violet.4" opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" disabled={inicio} />
+                <Burger color="violet.4" opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" disabled={inicio} />
+                LOTUS CLUB
+              </Group>
+              <Navbar/>
+            </Group>
+          </AppShell.Header>
+          <AppShell.Navbar p="md">
+            Navbar
+            {Array(15)
+              .fill(0)
+              .map((_, index) => (
+                <Skeleton key={index} h={28} mt="sm" animate={false} />
+              ))}
+          </AppShell.Navbar>
+          <AppShell.Main>
+            <Routes>
+              <Route path="/login" element={<Login/>} />
+              <Route path="/" element={<Dashboard/>} />
+              <Route path="/proveedor" element={<ProtectedRoute><Proveedor/></ProtectedRoute>} />
+              <Route path="/inventario" element={<ProtectedRoute><Inventario/></ProtectedRoute>} />
+              <Route path="/personal" element={<ProtectedRoute><Personal/></ProtectedRoute>} />
+              <Route path="/pedido" element={<ProtectedRoute><Pedido/></ProtectedRoute>} />
+              <Route path="/caja" element={<ProtectedRoute><Caja/></ProtectedRoute>} />
+              <Route path="/control" element={<ProtectedRoute><Control/></ProtectedRoute>} />
+              <Route path="/parametrica" element={<ProtectedRoute><Parametrica/></ProtectedRoute>} />
+              <Route path="*" element={<h1>404 Página no encontrada</h1>} />
+            </Routes>
+          </AppShell.Main>
+        </AppShell>
       </MantineProvider>
     </AuthProvider>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { ActionIcon, Group } from '@mantine/core';
+import { UserAuth } from '../context/AuthContext';
+import { IconPower } from '@tabler/icons-react';
+
+const Navbar = () => {
+  const { user } = UserAuth();
+  return (
+    <Group h="100%" px="md" style={{ justifyContent: 'space-between' }}>
+      <strong>{user?.cuenta || 'Anónimo'} - {user?.rol || 'Desconocido'}</strong>
+      <ActionIcon variant="filled" aria-label="Settings">
+        <IconPower style={{ width: '70%', height: '70%' }} stroke={1.5} />
+      </ActionIcon>
+    </Group>
+  )
+}
+
+export default Navbar
